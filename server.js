c094288d61cc5fb9c4ed4f53bc352dd84786dae2
amoljore7 <amoljore7@gmail.com>
2020-07-01 12:13:51 +0530

    connecting DB to app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -14,10 +17,10 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, {useNewUrlParser:true})
+mongoose.connect(db, {useNewUrlParser:true, useUnifiedTopology: true,})
 .then(() => console.log('>>>>>>>>>Mongo DB Connected...'))
 .catch(err => console.log(">>>>>>>>>>>>",+err));
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
