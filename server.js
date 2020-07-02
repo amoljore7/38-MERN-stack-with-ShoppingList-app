9fb40451c9f25155ebf38e424291f159f070067b
amoljore7 <amoljore7@gmail.com>
2020-07-02 16:42:18 +0530

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 const items = require("./routes/api/items");
 
@@ -27,6 +28,16 @@ mongoose
 
 // Use routes
 app.use("/api/items", items);
+
+// Serve static assests if in production - for deploye purpose
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
