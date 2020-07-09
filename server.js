a000974bcc516a93b2c58896b3c4824f36bad19c
amoljore7 <amoljore7@gmail.com>
2020-07-09 10:24:12 +0530

    Login and access control functionality done

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-const path = require('path');
-const config = require('config');
-
+const path = require("path");
+const config = require("config");
 
 const app = express();
 
@@ -11,30 +10,31 @@ const app = express();
 app.use(express.json());
 
 // DB Config - we nee to npm i config
-const DB = config.get('mongoURI');
+const DB = config.get("mongoURI");
 
 // Connect to MongoDB
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Mongo DB Connected..."))
   .catch((err) => console.log(">>>>> Mongo DB Not-Connected >>>>>>>", +err));
 
 // Use routes - All API From Route Folder
 app.use("/api/items", require("./routes/api/items"));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 // Serve static assests if in production - for deploye purpose
-if(process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
